c4f35e9fa14f9080bd7b2afdaadf0558f01759df
ShantanuNy <shantanunyaharkar@gmail.com>
2023-01-31 23:22:50 -0500

    last commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	images: {
+		domains: ["links.papareact.com"],
+	},
+	env: {
+		mapbox_key:
+			"pk.eyJ1Ijoic2hhbnRhbnUxOTk2IiwiYSI6ImNsZGgzcDFpeDBnbmozb2xocmNuemg3YTIifQ.eEAGYlYnf5CY3Ainq9JKkg",
+	},
+};
